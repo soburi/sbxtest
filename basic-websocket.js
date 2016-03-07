8c837ba96d678af68319cea682694a1d1fcbdf3a
soburi <soburi@gmail.com>
2016-03-07 23:19:25 +0900

    update

--- basic-websocket.js
+++ basic-websocket.js
@@ -2,10 +2,9 @@
 /* Sayamindu Dasgupta <sayamindu@media.mit.edu>, May 2014 */
 
 new (function() {
-	var ext = this;
+    var ext = this;
 
     var addr_name = {};
-    var name_addr = {};
     var ws_conn = {};
 
     // Cleanup function when the extension is unloaded
@@ -18,38 +17,39 @@ new (function() {
     };
 
     ext.connect = function(destHost) {
-	    ws_conn[destHost] = new WebSocket('ws://' + destHost);
-	    ws_conn[destHost].onerror = function(err) {
-	    };
+        var ws = new WebSocket('ws://' + destHost);
+        ws.onerror = function(err) {
+        };
 
-	    ws_conn[destHost].message_received = false;
-	    ws_conn[destHost].message_received = false;
+        ws.onmessage = function(msg) {
+            ws.message_received = true;
+            ws.message = msg;
+        };
 
-	    ws_conn[destHost].onmessage = function(msg) {
-		    ws_conn[destHost].message_received = true;
-		    ws_conn[destHost].message = msg;
-	    };
+        ws.message_received = false;
+
+        ws_conn[destHost] = ws;
     };
 
     ext.send = function(data, destHost) {
-	    ws_conn[destHost].send(data);
+        ws_conn[destHost].send(data);
     };
 
     ext.onMessageReceived = function(destHost) {
-	    if(ws_conn[destHost].message_received) {
-	    	ws_conn[destHost].message_received = false;
-			return true;
-	    }
-	    return false;
+        if(ws_conn[destHost].message_received) {
+            ws_conn[destHost].message_received = false;
+            return true;
+        }
+        return false;
     };
 
     ext.getMessage = function(destHost) {
-	    if(ws_conn[destHost].message) {
-	    	var ret = ws_conn[destHost].message.data;
-	    	ws_conn[destHost].message = null;
-			return ret;
-	    }
-	    return "";
+        if(ws_conn[destHost].message) {
+            var ret = ws_conn[destHost].message.data;
+            ws_conn[destHost].message = null;
+            return ret;
+        }
+        return "";
     };
    
     // Block and block menu descriptions
@@ -57,11 +57,11 @@ new (function() {
         blocks: [
             ['', 'connect to Host:%s', 'connect'],
             ['', 'send %s to %s', 'send'],
-            ['h', 'when message receive from %s', 'onMessageReceived'],
-			['r', 'message from %s', 'getMessage']
+            ['h', 'when data received from %s', 'onMessageReceived'],
+            ['r', 'data from %s', 'getMessage']
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('WebSocket extension', descriptor, ext);
+    ScratchExtensions.register('BasicWebSocket extension', descriptor, ext);
 })();
