8e3352bd285403ef64b516214ee2c6d7d322c5ec
soburi <soburi@gmail.com>
2016-03-07 22:31:23 +0900

    update

--- basic-websocket.js
+++ basic-websocket.js
@@ -17,21 +17,48 @@ new (function() {
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.connect = function(ipaddr, nodename) {
-	    addr_name[ipaddr] = nodename;
-	    name_addr[nodename] = ipaddr;
-	    ws_conn[ipaddr] = new WebSocket('ws://' + ipaddr);
+    ext.connect = function(destHost) {
+	    ws_conn[destHost] = new WebSocket('ws://' + destHost);
+	    ws_conn[destHost].onerror = function(err) {
+	    };
+
+	    ws_conn[destHost].message_received = false;
+	    ws_conn[destHost].message_received = false;
+
+	    ws_conn[destHost].onmessage = function(msg) {
+		    ws_conn[destHost].message_received = true;
+		    ws_conn[destHost].message = msg;
+	    };
     };
 
-    ext.send = function(data, node) {
-         
-    }
+    ext.send = function(data, destHost) {
+	    ws_conn[destHost].send(data);
+    };
 
+    ext.onMessageReceived = function(destHost) {
+	    if(ws_conn[destHost].message_received) {
+	    	ws_conn[destHost].message_received = false;
+		return true;
+	    }
+	    return false;
+    };
+
+    ext.getMessage = function(destHost) {
+	    if(ws_conn[destHost].message) {
+	    	var ret = ws_conn[destHost].message;
+	    	ws_conn[destHost].message = null;
+		return ret;
+	    }
+	    return "";
+    };
+   
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['', 'connect to IPAddress:%s as NodeName:%s', 'connect'],
-            ['', 'send %s to %s', 'send']
+            ['', 'connect to Host:%s', 'connect'],
+            ['', 'send %s to %s', 'send'],
+            ['h', 'when message receive from %s', 'onMessageReceived'],
+	    ['r', 'message from %s', 'getMessage']
         ]
     };
 
