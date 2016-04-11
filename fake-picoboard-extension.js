c842c7ec09b9d840f27b8b484ff0367d49b56544
soburi <soburi@gmail.com>
2016-04-11 23:53:30 +0900

    update

--- fake-picoboard-extension.js
+++ fake-picoboard-extension.js
@@ -2,34 +2,35 @@ new (function() {
     var ext = this;
 
     var ws_conn = {};
-    var last_message = null;
     var message_received = false;
+    var last_message_origin = null;
+    //var last_message = null;
+    var status_ = {status: 2, msg: 'Ready'};
+    var state_cache = {};
+    var reqid = 0;
 
     function get_first_or_value(_k) {
         var ret = this[_k];
-        if(ret == undefined) {
-            for(var kk in this) {
-                if(kk.indexOf('://') != -1) {
-                    return this[kk];
-                }
+        if(ret != undefined)
+            return ret;
+
+        for(var kk in this) {
+            if(kk.indexOf('://') != -1) {
+                return this[kk];
             }
         }
-        return ret;
     }
     ws_conn.get_ = get_first_or_value.bind(ws_conn);
 
     ext._shutdown = function() {
-        for(k in ws_conn) {
-            ws_conn[k].close();
-        }
+        for(k in ws_conn) ws_conn[k].close();
     };
 
-    var status_ = {status: 2, msg: 'Ready'};
     ext._getStatus = function() {
         return status_;
     };
 
-    ext.connect = function(_url) {
+    ext.connect = function(_url, callback) {
         if(_url in ws_conn) {
             switch(ws_conn[_url].readyState) {
                 case 0:
@@ -41,62 +42,32 @@ new (function() {
         }
 
         var reason = "";
+        var ws = null;
         try {
-            var ws = new WebSocket(_url);
+            ws = new WebSocket(_url);
             ws.message = null;
             ws_conn[_url] = ws;
+        }
+        catch(e) {
+            status_.status = 1;
+            status_.msg = _url + ' exception: ' + e.message;
+            callbacked = true;
+            callback(-1);
+            return;
+        }
 
-            ws.addEventListener('message', function(msg) {
-                message_received = true;
-                ws.message = msg;
-                last_message = msg;
-            });
-            
-            ws.addEventListener('close', function(event) {
-                // See http://tools.ietf.org/html/rfc6455#section-7.4.1
-                if (event.code == 1000)
-                    reason = "Normal closure, meaning that the purpose for which the connection was established has been fulfilled.";
-                else if(event.code == 1001)
-                    reason = "An endpoint is \"going away\", such as a server going down or a browser having navigated away from a page.";
-                else if(event.code == 1002)
-                    reason = "An endpoint is terminating the connection due to a protocol error";
-                else if(event.code == 1003)
-                    reason = "An endpoint is terminating the connection because it has received a type of data it cannot accept (e.g., an endpoint that understands only text data MAY send this if it receives a binary message).";
-                else if(event.code == 1004)
-                    reason = "Reserved. The specific meaning might be defined in the future.";
-                else if(event.code == 1005)
-                    reason = "No status code was actually present.";
-                else if(event.code == 1006)
-                   reason = "The connection was closed abnormally, e.g., without sending or receiving a Close control frame";
-                else if(event.code == 1007)
-                    reason = "An endpoint is terminating the connection because it has received data within a message that was not consistent with the type of the message (e.g., non-UTF-8 [http://tools.ietf.org/html/rfc3629] data within a text message).";
-                else if(event.code == 1008)
-                    reason = "An endpoint is terminating the connection because it has received a message that \"violates its policy\". This reason is given either if there is no other sutible reason, or if there is a need to hide specific details about the policy.";
-                else if(event.code == 1009)
-                   reason = "An endpoint is terminating the connection because it has received a message that is too big for it to process.";
-                else if(event.code == 1010) // Note that this status code is not used by the server, because it can fail the WebSocket handshake instead.
-                    reason = "An endpoint (client) is terminating the connection because it has expected the server to negotiate one or more extension, but the server didn't return them in the response message of the WebSocket handshake. <br /> Specifically, the extensions that are needed are: " + event.reason;
-                else if(event.code == 1011)
-                    reason = "A server is terminating the connection because it encountered an unexpected condition that prevented it from fulfilling the request.";
-                else if(event.code == 1015)
-                    reason = "The connection was closed due to a failure to perform a TLS handshake (e.g., the server certificate can't be verified).";
-                else
-                    reason = "Unknown reason";
+        var callbacked = false;
+        setTimeout( function() {
+            status_.status = 1;
+            status_.msg = "Connect timeout";
 
-                if(event.code != 1000) {
-                    status_.status = 1;
-                    status_.msg = _url + ': ' + reason;
-                    event.target.close_status_ = event.code;
-                    event.target.close_reason_ = reason;
-                }
+            if(!callbacked) {
+                callbacked = true;
+                callback(-1);
+            }
+        }, 3000 );
 
-            });
-
-            ws.addEventListener('error', function(err) {
-                status_.status = 1;
-                status_.msg = 'onerror: ' + reason;
-            });
-            
+        ws.addEventListener('open', function(event) {
             var msg = "";
             var check = false;
             for(k in ws_conn) {
@@ -114,11 +85,73 @@ new (function() {
                 status_.status = 2;
                 status_.msg = 'Ready';
             }
-        }
-        catch(e) {
+
+            if(!callbacked) {
+                callbacked = true;
+                callback();
+            }
+        });
+
+        ws.addEventListener('error', function(err) {
             status_.status = 1;
-            status_.msg = _url + ' exception: ' + e.message;
-        }
+            status_.msg = 'onerror: ' + reason;
+
+            if(!callbacked) {
+                callbacked = true;
+                callback(-1);
+            }
+        });
+        
+        ws.addEventListener('message', function(event) {
+            message_received = true;
+            ws.message = event;
+            last_message_origin = ws.url;
+            //last_message = event;
+        });
+        
+        ws.addEventListener('close', function(event) {
+            // See http://tools.ietf.org/html/rfc6455#section-7.4.1
+            if (event.code == 1000)
+                reason = "Normal closure, meaning that the purpose for which the connection was established has been fulfilled.";
+            else if(event.code == 1001)
+                reason = "An endpoint is \"going away\", such as a server going down or a browser having navigated away from a page.";
+            else if(event.code == 1002)
+                reason = "An endpoint is terminating the connection due to a protocol error";
+            else if(event.code == 1003)
+                reason = "An endpoint is terminating the connection because it has received a type of data it cannot accept (e.g., an endpoint that understands only text data MAY send this if it receives a binary message).";
+            else if(event.code == 1004)
+                reason = "Reserved. The specific meaning might be defined in the future.";
+            else if(event.code == 1005)
+                reason = "No status code was actually present.";
+            else if(event.code == 1006)
+               reason = "The connection was closed abnormally, e.g., without sending or receiving a Close control frame";
+            else if(event.code == 1007)
+                reason = "An endpoint is terminating the connection because it has received data within a message that was not consistent with the type of the message (e.g., non-UTF-8 [http://tools.ietf.org/html/rfc3629] data within a text message).";
+            else if(event.code == 1008)
+                reason = "An endpoint is terminating the connection because it has received a message that \"violates its policy\". This reason is given either if there is no other sutible reason, or if there is a need to hide specific details about the policy.";
+            else if(event.code == 1009)
+               reason = "An endpoint is terminating the connection because it has received a message that is too big for it to process.";
+            else if(event.code == 1010) // Note that this status code is not used by the server, because it can fail the WebSocket handshake instead.
+                reason = "An endpoint (client) is terminating the connection because it has expected the server to negotiate one or more extension, but the server didn't return them in the response message of the WebSocket handshake. <br /> Specifically, the extensions that are needed are: " + event.reason;
+            else if(event.code == 1011)
+                reason = "A server is terminating the connection because it encountered an unexpected condition that prevented it from fulfilling the request.";
+            else if(event.code == 1015)
+                reason = "The connection was closed due to a failure to perform a TLS handshake (e.g., the server certificate can't be verified).";
+            else
+                reason = "Unknown reason";
+
+            if(event.code != 1000) {
+                status_.status = 1;
+                status_.msg = _url + ': ' + reason;
+                ws.close_status_ = event.code;
+                ws.close_reason_ = reason;
+
+                if(!callbacked) {
+                    callbacked = true;
+                    callback(-1);
+                }
+            }
+        });
     };
 
     ext.disconnect = function(_url) {
@@ -136,6 +169,10 @@ new (function() {
     };
 
     ext.getMessage = function(_url) {
+        var ws = ws_conn.get_(_url);
+        return ws.message;
+
+        /*
         if(_url in ws_conn && ws_conn[_url].message != null) {
             var ret = ws_conn[_url].message.data;
             if(last_message === ws_conn[_url].message) {
@@ -145,19 +182,23 @@ new (function() {
             return ret;
         }
         return null;
+        */
     };
 
-    ext.getLastReceivedMessage = function() {
-        var tmp = last_message;
-        last_message = null;
-        return tmp;
+    ext.getLastReceivedMessage = function(_url) {
+        var ws = ws_conn.get_(_url);
+        return ws.message;
+        //var tmp = last_message;
+        //last_message = null;
+        //return tmp;
     };
     
     ext.getLastReceivedMessageOrigin = function() {
-        if(last_message != null) {
-            return last_message.origin;
-        }
-        return null;
+        return last_message_origin;
+        //if(last_message != null) {
+        //    return last_message.origin;
+        //}
+        //return null;
     };
 
     ext.onMessageReceived = function() {
@@ -167,6 +208,8 @@ new (function() {
         }
         return false;
     };
+
+    // Tiny Json Library
 
     ext.emptyObject = function() {
         var obj = new Object();
@@ -200,9 +243,7 @@ new (function() {
         return null;
     };
 
-    var reqid = 0;
-
-    var request_value = function(prop, callback) {
+    ext.getSensorValue = function(prop, callback) {
         var ws = ws_conn.get_(null);
         var req = {"request": prop, "reqid":reqid};
 
@@ -212,31 +253,29 @@ new (function() {
             if(!callbacked) {
                 callbacked = true;
                 callback();
+                status_.status = 1;
+                status_.msg = "Request timeout";
             }
         }, 3000 );
 
-        ws.addEventListener('message', function(message) {
-            var resp = JSON.parse(message.data);
-	    var _req = req;
+        ws.addEventListener('message', function(event) {
+            var resp = JSON.parse(event.data);
+            var _req = req;
             if(!callbacked) {
                 if(resp.response == prop) {
                     callbacked = true;
-                    callback("" + resp.value);
+                    callback(resp.value);
                 }
             }
         });
 
         ext.send(JSON.stringify(req), null);
-	reqid++;
+        reqid++;
     };
 
-    ext.getButtonStatus = request_value;
-    ext.getSensorValue = request_value;
 
-    var state_cache = {};
-
-    var state_received = function(message) {
-        var resp = JSON.parse(message.data);
+    var state_received = function(event) {
+        var resp = JSON.parse(event.data);
         var oldval = state_cache[resp.notify];
         state_cache[resp.notify] = { update: (oldval == resp.value), value: resp.value };
     };
@@ -268,9 +307,9 @@ new (function() {
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            [ '', 'connect to %s', 'connect'],
+            ['w', 'connect to %s', 'connect'],
             [ '', 'disconnect', 'disconnect'],
-            ['R', 'sensor %m.buttonStatus sensor value', 'getButtonStatus'],
+            ['R', 'sensor %m.buttonStatus sensor value', 'getSensorValue'],
             ['R', '%m.sensorType sensor value', 'getSensorValue'],
             ['h', 'when %m.buttonStatus', 'onButtonChanged'],
             ['h', 'when %m.sensorType %m.lessMore %n', 'onSensorValueChanged'],
