141cece3872d32d117a852691ba96ca58aa1da1e
soburi <soburi@gmail.com>
2016-04-17 11:48:36 +0900

    update

---
+++ websocket-extension.js
@@ -0,0 +1,30 @@
+new (function() {
+    var src = $(document).createElement('script');
+    src.src = './ws-ext.js';
+    $(document).getElementsByTagName("head")[0].appendChild(scr);
+
+    $(document).ready( function() {
+
+        wsext(this);
+
+        // Block and block menu descriptions
+        var descriptor = {
+        blocks: [
+            ['w', 'connect to %s', 'connect'],
+            ['w', 'disconnect %s', 'disconnect'],
+            [ '', 'send %s to %s', 'send'],
+            ['R', 'message from %s', 'getMessage'],
+            ['r', 'message origin', 'getLastReceivedMessageOrigin'],
+            ['h', 'when data received', 'onMessageReceived'],
+
+            ['r', 'JSON', 'emptyObject'],
+            ['r', 'add string %s:%s to %s', 'addJsonProperty'],
+            ['r', 'add number %s:%n to %s', 'addJsonProperty'],
+            ['r', 'get property:%s from JSON:%s', 'getJsonProperty'],
+        ]
+        };
+
+        // Register the extension
+        ScratchExtensions.register('WebSocket extension', descriptor, ext);
+    });
+})();
