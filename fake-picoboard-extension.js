2111740f40063d14410c56981cd6ea4ca8cda54d
soburi <soburi@gmail.com>
2016-04-17 15:36:14 +0900

    update

--- fake-picoboard-extension.js
+++ fake-picoboard-extension.js
@@ -112,18 +112,15 @@ new (function() {
         ScratchExtensions.register(name, descriptor, ext);
     };
 
-    if(document.fake_picoboard_ext_init == undefined) {
-            document.fake_picoboard_ext_init = fake_picoboard_ext_init;
-    }
     if(document.ws_ext_init == undefined) {
         var scriptpath = document.currentScript.src.match(/.*\//);
         $.getScript(scriptpath + 'ws-ext.js', function(wsext, textStatus, jqxhr) {
             document.ws_ext_init = ws_ext_init;
-            document.fake_picoboard_ext_init(ws_ext_init);
+            fake_picoboard_ext_init(ws_ext_init);
         });
     }
     else {
-        document.fake_picoboard_ext_init(ws_ext_init);
+        fake_picoboard_ext_init(ws_ext_init);
     }
 
 })();
