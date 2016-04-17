65ddb11ab19c3183b57daa18382c5de8e872e010
soburi <soburi@gmail.com>
2016-04-17 11:16:06 +0900

    update

--- websocket-extension.js
+++ websocket-extension.js
@@ -2,35 +2,61 @@ new (function() {
     var ext = this;
 
     var ws_conn = {};
-    var last_message = null;
-    var message_received = false;
+    var received_events = [];
+    var received_events_length = 20;
+    var status_ = {status: 2, msg: 'Ready'};
+    var state_cache = {};
+    var reqid = 0;
 
-    function get_first_or_value(_k) {
+    ws_conn.get_ = function(_k) {
         var ret = this[_k];
-        if(ret == undefined) {
-            for(var kk in this) {
-                if(kk.indexOf('://') != -1) {
-                    return this[kk];
+        if(ret != undefined)
+            return ret;
+
+        for(var kk in this) {
+            if(kk.indexOf('://') != -1) {
+                return this[kk];
+            }
+        }
+        return null;
+    }.bind(ws_conn);
+
+    ws_conn.getErrorReason = function() {
+        var msg = null;
+        var ws_conn = this;
+        for(k in ws_conn) {
+            var ws = ws_conn[k];
+            if( ws.close_status_ != undefined) {
+                if(msg == null) {
+                    msg = ws.url + ': ' + ws.close_reason_;
+                }
+                else {
+                    msg += '\n' + ws.url + ': ' + ws.close_reason_;
                 }
             }
         }
-        return ret;
-    }
-    ws_conn.get_ = get_first_or_value.bind(ws_conn);
+        return msg;
+    }.bind(ws_conn);
+
+    received_events.unchecked = function() {
+        for(var i=this.length-1; i>=0; i--) {
+            if(this[i].checked == undefined) return this[i];
+        }
+        return null;
+    }.bind(received_events);
 
     ext._shutdown = function() {
-        for(k in ws_conn) {
-            ws_conn[k].close();
-        }
+        for(k in ws_conn) ws_conn[k].close();
     };
 
-    var status_ = {status: 2, msg: 'Ready'};
     ext._getStatus = function() {
         return status_;
     };
 
-    ext.connect = function(_url) {
+    ext.connect = function(_url, callback) {
+        console.log("ext.connect: %s %O", _url, callback);
         if(_url in ws_conn) {
+            console.log("ext.connect: %s readyState:%d", _url, ws_conn[_url].readyState);
             switch(ws_conn[_url].readyState) {
                 case 0:
                 case 1:
@@ -41,139 +67,171 @@ new (function() {
         }
 
         var reason = "";
+        var ws = null;
         try {
-            var ws = new WebSocket(_url);
+            ws = new WebSocket(_url);
             ws.message = null;
             ws_conn[_url] = ws;
-
-            ws.onmessage = function(msg) {
-                message_received = true;
-                ws.message = msg;
-                last_message = msg;
-            };
-            
-            ws.onclose = function(event) {
-                // See http://tools.ietf.org/html/rfc6455#section-7.4.1
-                if (event.code == 1000)
-                    reason = "Normal closure, meaning that the purpose for which the connection was established has been fulfilled.";
-                else if(event.code == 1001)
-                    reason = "An endpoint is \"going away\", such as a server going down or a browser having navigated away from a page.";
-                else if(event.code == 1002)
-                    reason = "An endpoint is terminating the connection due to a protocol error";
-                else if(event.code == 1003)
-                    reason = "An endpoint is terminating the connection because it has received a type of data it cannot accept (e.g., an endpoint that understands only text data MAY send this if it receives a binary message).";
-                else if(event.code == 1004)
-                    reason = "Reserved. The specific meaning might be defined in the future.";
-                else if(event.code == 1005)
-                    reason = "No status code was actually present.";
-                else if(event.code == 1006)
-                   reason = "The connection was closed abnormally, e.g., without sending or receiving a Close control frame";
-                else if(event.code == 1007)
-                    reason = "An endpoint is terminating the connection because it has received data within a message that was not consistent with the type of the message (e.g., non-UTF-8 [http://tools.ietf.org/html/rfc3629] data within a text message).";
-                else if(event.code == 1008)
-                    reason = "An endpoint is terminating the connection because it has received a message that \"violates its policy\". This reason is given either if there is no other sutible reason, or if there is a need to hide specific details about the policy.";
-                else if(event.code == 1009)
-                   reason = "An endpoint is terminating the connection because it has received a message that is too big for it to process.";
-                else if(event.code == 1010) // Note that this status code is not used by the server, because it can fail the WebSocket handshake instead.
-                    reason = "An endpoint (client) is terminating the connection because it has expected the server to negotiate one or more extension, but the server didn't return them in the response message of the WebSocket handshake. <br /> Specifically, the extensions that are needed are: " + event.reason;
-                else if(event.code == 1011)
-                    reason = "A server is terminating the connection because it encountered an unexpected condition that prevented it from fulfilling the request.";
-                else if(event.code == 1015)
-                    reason = "The connection was closed due to a failure to perform a TLS handshake (e.g., the server certificate can't be verified).";
-                else
-                    reason = "Unknown reason";
-
-                if(event.code != 1000) {
-                    status_.status = 1;
-                    status_.msg = _url + ': ' + reason;
-                    event.target.close_status_ = event.code;
-                    event.target.close_reason_ = reason;
-                }
-
-            };
-
-            ws.onerror = function(err) {
-                status_.status = 1;
-                status_.msg = 'onerror: ' + reason;
-            };
-            
-            var msg = "";
-            var check = false;
-            for(k in ws_conn) {
-                var ws = ws_conn[k];
-                if( ws.close_status_ != undefined && ws.close_status_ != 1000) {
-                    check = true;
-                    msg += ws.url + ': ' + ws.close_reason_ + '\n';
-                }
-            }
-            if(check) {
-                status_.status = 1;
-                status_.msg = msg;
-            }
-            else {
-                status_.status = 2;
-                status_.msg = 'Ready';
-            }
         }
         catch(e) {
             status_.status = 1;
             status_.msg = _url + ' exception: ' + e.message;
+            callbacked = true;
+            console.log("ext.connect: %s: exception:%o", _url, e);
+            callback();
+            return;
         }
+
+        var callbacked = false;
+        setTimeout( function() {
+            if(!callbacked) {
+                status_.status = 1;
+                status_.msg    = "Connect timeout";
+                callbacked = true;
+                console.log("ext.connect: %s timeout", _url);
+                callback();
+            }
+        }, 3000 );
+
+        ws.addEventListener('open', function(event) {
+            console.log("%s: onopen", _url);
+
+            if(!callbacked) {
+                var errmsg = ws_conn.getErrorReason();
+                status_.status = (errmsg != null) ? 1 : 2;
+                status_.msg    = (errmsg != null) ? errmsg : 'Ready';
+                callbacked = true;
+                console.log("%s: onopen: callback:%s", _url, status_.msg);
+                callback();
+            }
+        });
+
+        ws.addEventListener('error', function(err) {
+            console.log("%s: onerror", ws.url);
+            if(!callbacked) {
+                status_.status = 1;
+                status_.msg    = 'onerror: ' + reason;
+                callbacked = true;
+                console.log("%s: onerror: msg:%s", _url, status_.msg);
+                callback();
+            }
+        });
+        
+        ws.addEventListener('message', function(event) {
+            console.log("%s: onmessage:", ws.url, event.data);
+            if(received_events.length = received_events_length) {
+                received_events.shift();
+            }
+            received_events.push(event);
+        });
+        
+        ws.addEventListener('close', function(event) {
+            console.log("%s: onclose: %d", ws.url, event.code);
+                 if(event.code == 1001) reason = "1000: CLOSE_NORMAL";
+            else if(event.code == 1001) reason = "1001: CLOSE_GOING_AWAY";
+            else if(event.code == 1002) reason = "1002: CLOSE_PROTOCOL_ERROR";
+            else if(event.code == 1003) reason = "1003: CLOSE_UNSUPPORTED";
+            else if(event.code == 1004) reason = "1004: RESERVED";
+            else if(event.code == 1005) reason = "1005: CLOSE_NO_STATUS";
+            else if(event.code == 1006) reason = "1006: CLOSE_ABNORMAL";
+            else if(event.code == 1007) reason = "1007: Unsupported Data";
+            else if(event.code == 1008) reason = "1008: Policy Violation";
+            else if(event.code == 1009) reason = "1009: CLOSE_TOO_LARGE";
+            else if(event.code == 1010) reason = "1010: Missing Extension";
+            else if(event.code == 1011) reason = "1011: Internal Error";
+            else if(event.code == 1012) reason = "1012: Service Restart";
+            else if(event.code == 1013) reason = "1013: Try Again Later";
+            else if(event.code == 1014) reason = "1014: RESERVED";
+            else if(event.code == 1015) reason = "1015: TLS Handshake";
+            else                        reason = "" + event.code + ": Unknown reason";
+
+            if(event.code != 1000) {
+                ws.close_status_ = event.code;
+                ws.close_reason_ = reason;
+
+                status_.status = 1;
+                status_.msg = ws_conn.getErrorReason();
+
+                if(!callbacked) {
+                    callbacked = true;
+                    console.log("%s: onclose: msg:%s", _url, status_.msg);
+                    callback();
+                }
+            }
+        });
     };
 
-    ext.disconnect = function(_url) {
-        var ws = ws_conn.get_(_url);
-        switch(ws.readyState) {
-            case 0:
-            case 1:
-            ws.close();
-        }
+    ext.disconnect = function(arg0, arg1) {
+        var disconnect_ = function(_url, callback) {
+            var ws = ws_conn.get_(_url);
+            if(ws == null) {
+                console.log("ext.disconnect: callback %s not yet init", _url);
+                callback();
+                return;
+            }
+
+            switch(ws.readyState) {
+                case 0:
+                case 1:
+                    console.log("ext.disconnect: close: %s readyState:%d", ws.url, ws.readyState);
+                    ws.close();
+                    ws.addEventListener('close', function(event) {
+                        console.log("%s: onclose callback", ws.url);
+                        callback();
+                        return;
+                    });
+            }
+            console.log("ext.disconnect: %s: callback default", ws.url);
+            callback();
+        };
+        disconnect_(  arg1==undefined ? null : arg0, arg1==undefined ? arg0 : arg1 );
     };
 
     ext.send = function(data, _url) {
         var ws = ws_conn.get_(_url);
+        console.log("ext.send: %s, %s %o", _url, ws.url, data);
         ws.send(data);
     };
 
     ext.getMessage = function(_url) {
-        if(_url in ws_conn && ws_conn[_url].message != null) {
-            var ret = ws_conn[_url].message.data;
-            if(last_message === ws_conn[_url].message) {
-                last_message = null;
+        console.log("ext.getMessage: %s", _url);
+        for(var i=0; i<received_events.length; i++) {
+            if(received_events[i].checked != defined && received_events[i].target.url == _url) {
+                var r = received_events.splice(i, 1);
+                return r[0];
             }
-            ws_conn[_url].message = null;
-            return ret;
         }
-        return null;
+
+        return null; 
     };
 
-    ext.getLastReceivedMessage = function() {
-        var tmp = last_message;
-        last_message = null;
-        return tmp;
-    };
-    
     ext.getLastReceivedMessageOrigin = function() {
-        if(last_message != null) {
-            return last_message.origin;
-        }
-        return null;
+        console.log("ext.getLastReceivedMessageOrigin");
+        if(received_event.length == 0) 
+            return null;
+        else
+            return received_event[0].target.url;
     };
 
     ext.onMessageReceived = function() {
-        if(message_received == true) {
-            message_received = false;
+        var chk = received_events.unchecked();
+        if(chk != null) {
+            chk.checked = true;
             return true;
         }
         return false;
     };
 
+    // Tiny Json Library
+
     ext.emptyObject = function() {
+        console.log("ext.emptyObject");
         var obj = new Object();
         return JSON.stringify(obj);
     };
 
     ext.addJsonProperty = function(propname, propvalue, jsonstr) {
+        console.log("ext.addJsonProperty: %s %s %o", propname, propvalue, jsonstr);
         var jsonobj = jsonstr;
         if(jQuery.type(jsonstr) == 'string') {
             jsonobj = JSON.parse(jsonstr);
@@ -184,6 +242,7 @@ new (function() {
     };
 
     ext.getJsonProperty = function(propname, jsonstr) {
+        console.log("ext.getJsonProperty: %s %o", propname, jsonstr);
         var jsonobj = jsonstr;
         if(jQuery.type(jsonstr) == 'string') {
             jsonobj = JSON.parse(jsonstr);
@@ -203,12 +262,11 @@ new (function() {
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            [ '', 'connect to %s', 'connect'],
-            [ '', 'disconnect %s', 'disconnect'],
+            ['w', 'connect to %s', 'connect'],
+            ['w', 'disconnect %s', 'disconnect'],
             [ '', 'send %s to %s', 'send'],
-            ['r', 'message from %s', 'getMessage'],
+            ['R', 'message from %s', 'getMessage'],
             ['r', 'message origin', 'getLastReceivedMessageOrigin'],
-            ['r', 'last received message', 'getLastReceivedMessage'],
             ['h', 'when data received', 'onMessageReceived'],
 
             ['r', 'JSON', 'emptyObject'],
