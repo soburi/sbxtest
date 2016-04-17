f76825ef6a9bcb6e71fffb337830ca822dd7df08
soburi <soburi@gmail.com>
2016-04-17 11:22:04 +0900

    update

--- ws-ext.js
+++ ws-ext.js
@@ -1,12 +1,8 @@
-new (function() {
-    var ext = this;
-
+function(ext) {
     var ws_conn = {};
     var received_events = [];
     var received_events_length = 20;
     var status_ = {status: 2, msg: 'Ready'};
-    var state_cache = {};
-    var reqid = 0;
 
     ws_conn.get_ = function(_k) {
         var ret = this[_k];
@@ -258,24 +254,4 @@ new (function() {
         }
         return null;
     };
-
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            ['w', 'connect to %s', 'connect'],
-            ['w', 'disconnect %s', 'disconnect'],
-            [ '', 'send %s to %s', 'send'],
-            ['R', 'message from %s', 'getMessage'],
-            ['r', 'message origin', 'getLastReceivedMessageOrigin'],
-            ['h', 'when data received', 'onMessageReceived'],
-
-            ['r', 'JSON', 'emptyObject'],
-            ['r', 'add string %s:%s to %s', 'addJsonProperty'],
-            ['r', 'add number %s:%n to %s', 'addJsonProperty'],
-            ['r', 'get property:%s from JSON:%s', 'getJsonProperty'],
-        ]
-    };
-
-    // Register the extension
-    ScratchExtensions.register('WebSocket extension', descriptor, ext);
-})();
+}
