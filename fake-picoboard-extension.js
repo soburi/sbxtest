cf8e858b08b3d23ff8c04d6866f41b6cc3ae2a39
soburi <soburi@gmail.com>
2016-04-19 01:02:02 +0900

    update

--- fake-picoboard-extension.js
+++ fake-picoboard-extension.js
@@ -9,16 +9,15 @@ new (function() {
         blocks: [
             ['w', 'connect to %s', 'connect'],
             ['w', 'disconnect', 'disconnect'],
-            ['R', 'sensor %m.buttonStatus sensor value', 'getSensorValue'],
-            ['R', '%m.sensorType sensor value', 'getSensorValue'],
+            ['r', 'sensor %m.buttonStatus sensor value', 'getSensorBoolValue'],
+            ['r', '%m.sensorType sensor value', 'getSensorValue'],
             ['h', 'when %m.buttonStatus', 'onButtonChanged'],
             ['h', 'when %m.sensorType %m.lessMore %n', 'onSensorValueChanged'],
         ],
         menus: {
             lessMore: ['<', '>'],
             buttonStatus: ['button pressed', 'A connected', 'B connected', 'C connected', 'D connected'],
-            sensorType: ['slider'],
-            //sensorType: ['slider', 'light', 'sound', 'resistance-A', 'resistance-B', 'resistance-C', 'resistance-D'],
+            sensorType: ['slider', 'light', 'sound', 'resistance-A', 'resistance-B', 'resistance-C', 'resistance-D'],
         },
     };
 
@@ -26,48 +25,21 @@ new (function() {
 
         let state_cache = {};
 
-        ext.getSensorValue = function(prop, callback) {
-            console.log("ext.getSensorValue: %s %o", prop, callback);
-            let req = {"request": prop};
-
-            let ws = ext.getConnection(null);
-            if(ws == null) {
-                console.log("%s getSensorValue: not yet init", ws.url);
-                callback(-1);
-                return;
-            }
-
-            let callbacked = false;
-            
-            setTimeout( function() {
-                if(!callbacked) {
-                    callbacked = true;
-                    ext.setErrorStatus(1, "getSensorValue: Request timeout");
-                    console.log("%s: getSensorValue: timeout, callback", ws.url);
-                    callback(-1);
-                }
-            }, 3000 );
-
-            ext.addEventListener('message-received', function(event) {
-                let resp = JSON.parse(event.data);
-                if(!callbacked) {
-                    if(resp.response != undefined && resp.value != undefined && resp.response == prop) {
-                        callbacked = true;
-                        console.log("%s onmessage: getSensorValue callback:%d response:%o", ws.url, resp.value, resp);
-                        let state = state_cache[prop];
-                        if(state != null) {
-                            state_cache[prop].value = resp.value;
-                        }
-                        else {
-                            state_cache[prop] = { last_probed: resp.value, value: resp.value };
-                        }
-                        callback(resp.value);
-                    }
-                }
-            });
-
-            ext.send(JSON.stringify(req), null);
+        let proptable = {
+            'button pressed': "button",
+            'A connected': "connectorA",
+            'B connected': "connectorB",
+            'C connected': "connectorC",
+            'D connected': "connectorD",
+            'slider': "slider",
+            'light': "light",
+            'sound': "sound",
+            'resistance-A': "resistance-A",
+            'resistance-B': "resistance-B",
+            'resistance-C': "resistance-C",
+            'resistance-D': "resistance-D"
         };
+
 
         ext.isInternalProcessEvent = function(event) {
             let recv = JSON.parse(event.data);
@@ -79,7 +51,6 @@ new (function() {
             if(recv.notify != undefined) {
 
                 for(k in recv.notify) {
-
                     let state = state_cache[k];
                     if(state != null) {
                         state_cache[k].value = recv.notify[k];
@@ -92,17 +63,35 @@ new (function() {
             }
         });
 
+        ext.getSensorValue = function(prop) {
+            //console.log("ext.getSensorValue: %s %o", prop, callback);
+
+            let key = proptable[prop];
+
+            if(status_cache[key] == undefined) return "";
+            if(status_cache[key].value == undefined) return "";
+            
+            return state_cache[key].value;
+        };
+
+        ext.getSensorBoolValue = function(prop) {
+            return (ext.getSensorValue(prop) ? true : false);
+        };
+
         ext.onButtonChanged = function(prop) {
             //console.log("ext.onButtonChanged: %s", prop);
             let ws = ext.getConnection(null);
             if(ws == null) return false;
 
-            let key = "";
-            if(prop == "button pressed") key = "button";
+            let key = proptable[prop];
+            if(key == undefined) return false;
 
-            let last_probed = state_cache[key].last_probed;
-            let new_value = state_cache[key].value;
-            state_cache[key].last_probed = state_cache[key].value;
+            let state = state_cache[key];
+            if(state == undefined) return false;
+
+            let last_probed = state.last_probed;
+            let new_value = state.value;
+            state_cache[key].last_probed = state.value;
             if(last_probed != new_value && new_value == true) {
                 return true;
             }
@@ -114,9 +103,15 @@ new (function() {
             let ws = ext.getConnection(null);
             if(ws == null) return false;
 
-            let last_probed = state_cache[prop].last_probed;
-            let new_value = state_cache[prop].value;
-            state_cache[prop].last_probed = state_cache[prop].value;
+            let key = proptable[prop];
+            if(key == undefined) return false;
+
+            let state = state_cache[key];
+            if(state == undefined) return false;
+
+            let last_probed = state.last_probed;
+            let new_value = state.value;
+            state_cache[prop].last_probed = state.value;
             if(last_probed != new_value) {
                 if( (lessmore == '<' && last_probed >= threshold && new_value < threshold)  &&
                     (lessmore == '>' && last_probed <= threshold && new_value > threshold) ) {
