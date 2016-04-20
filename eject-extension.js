022ecfea0c591c41bafa7c370acd986bc3f7c236
soburi <soburi@gmail.com>
2016-04-20 09:40:40 +0900

    update

--- eject-extension.js
+++ eject-extension.js
@@ -1,3 +1,21 @@
+/*
+  Copyright (c) 2016 TOKITA Hiroshi.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 new (function() {
     let ext_ = this;
 
@@ -13,11 +31,21 @@ new (function() {
             [ '', 'close drive', 'send_close'],
             ['h', 'when disc ejected', 'onDiskEjected'],
             ['h', 'when drive closed', 'onDriveClosed'],
+            ['b', 'boolean', 'isBool'],
+            ['B', 'async boolean', 'isAsyncBool'],
         ]
     };
 
 
     let eject_ext_init = function(ext) {
+
+	ext.isBool = function() {
+		return true;
+	};
+
+	ext.isAsyncBool = function(callback) {
+		callback(true);
+	};
 
         ext.send_eject = function() {
             let data = {command: 'eject'};
